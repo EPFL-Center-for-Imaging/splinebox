45bcaf11c542e6d7437e01e2e1c883ce5858462b
faymanns <florian.aymanns@t-online.de>
2024-03-07 16:46:25 +0100

    Make compatible with H3 and HE3

--- tests/test_basis_functions.py
+++ tests/test_basis_functions.py
@@ -23,7 +23,14 @@ def test_1st_derivatives(basis_function):
     # estimated_derivative is inaccurate whenever there is a kink in the
     # basis function. These should be isolated values since there are never
     # two kinks right next to each other.
-    close = np.convolve(close, np.ones(2), mode="same")
+    kernel = np.ones(2)
+    if close.ndim == 2:
+        # the output of H3 and HE3 is 2D
+        for i in range(2):
+            close[i] = np.convolve(close[i], kernel, mode="same")
+    else:
+        close = np.convolve(close, kernel, mode="same")
+
     assert np.all(close > 0)
 
 
@@ -36,8 +43,10 @@ def test_2nd_derivatives(basis_function):
     dy = np.diff(y)
     estimated_derivative = dy / dx
 
-    if isinstance(basis_function, splinebox.basis_functions.B1):
-        # B1 is not differentiable twice.
+    if isinstance(
+        basis_function, (splinebox.basis_functions.B1, splinebox.basis_functions.H3, splinebox.basis_functions.HE3)
+    ):
+        # B1, H3, and HE3 are not differentiable twice.
         with pytest.raises(RuntimeError):
             basis_function.eval_2nd_derivative(x[:-1] + dx / 2)
     else:
