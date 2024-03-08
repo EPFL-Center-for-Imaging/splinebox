5ae6ba6cd208b58a62adf9c563c4e83a50d61d1a
faymanns <florian.aymanns@t-online.de>
2024-03-08 15:14:02 +0100

    Account for the different number of coefficients needed for closed and open splines

--- tests/test_spline_curves.py
+++ tests/test_spline_curves.py
@@ -18,17 +18,19 @@ def _not_differentiable_twice(spline_curve):
 def test_eval(spline_curve, coef_gen, derivative, eval_positions):
     support = spline_curve.basis_function.support
     half_support = support / 2
+    closed = spline_curve.closed
 
     with pytest.raises(RuntimeError):
         # coefficients have not been set
         spline_curve.eval(eval_positions, derivative=derivative)
 
     # Set coefficients
-    spline_curve.coefs = coef_gen(spline_curve.M, support)
+    spline_curve.coefs = coef_gen(spline_curve.M, support, closed)
 
     if _is_interpolating(spline_curve) and derivative == 0:
         values = spline_curve.eval(np.arange(spline_curve.M), derivative=derivative)
-        assert np.allclose(values, spline_curve.coefs[int(half_support) : -int(half_support)])
+        expected = spline_curve.coefs if closed else spline_curve.coefs[int(half_support) : -int(half_support)]
+        assert np.allclose(values, expected)
 
     if derivative == 2 and _not_differentiable_twice(spline_curve):
         with pytest.raises(RuntimeError):
