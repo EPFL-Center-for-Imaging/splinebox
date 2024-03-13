458f808930f814539177d99942718acfe844ed96
faymanns <florian.aymanns@t-online.de>
2024-03-13 10:04:16 +0100

    Test for multi dimensional codomains

--- tests/conftest.py
+++ tests/conftest.py
@@ -79,14 +79,27 @@ def eval_positions(request):
     return request.param
 
 
+@pytest.fixture(params=[1, 2, 3, 5])
+def codomain_dimensionality(request):
+    """
+    The number of dimensions the spline output
+    has. If the dimension is 2, a curve spline
+    describes a line in 2D space parameterized by
+    a single parameter.
+    """
+    return request.param
+
+
 @pytest.fixture
-def coef_gen():
+def coef_gen(codomain_dimensionality):
     rng = np.random.default_rng(seed=1492)
 
     def _point_gen(M, support, closed):
         if closed:
-            return rng.random(M) * 100
+            points = rng.random((M, codomain_dimensionality)) * 100
         else:
-            return rng.random(M + support) * 100
+            points = rng.random((M + support, codomain_dimensionality)) * 100
+        # remove superfluos dimension if codomain_dimensionlity is 1
+        return np.squeeze(points)
 
     return _point_gen
