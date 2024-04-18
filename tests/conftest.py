18305c5bfb715e3f40316eea0d059234cb2f83f0
faymanns <florian.aymanns@t-online.de>
2024-04-18 09:48:40 +0200

    Add separate test case for exponential spline with alpha=2pi/M

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,12 +12,22 @@ import splinebox.spline_curves
         (splinebox.basis_functions.B1, {}),
         (splinebox.basis_functions.B2, {}),
         (splinebox.basis_functions.B3, {}),
-        (splinebox.basis_functions.Exponential, {"M": 5, "alpha": 0.3}),
+        (splinebox.basis_functions.Exponential, {"M": 5, "alpha": 2 * np.pi / 5}),
+        (splinebox.basis_functions.Exponential, {"M": 10, "alpha": 0.3}),
         (splinebox.basis_functions.CatmullRom, {}),
         (splinebox.basis_functions.CubicHermite, {}),
         (splinebox.basis_functions.ExponentialHermite, {"alpha": 0.4}),
     ],
-    ids=["B1", "B2", "B3", "Exponential-M5-alpha0.3", "CatmullRom", "CubicHermite", "ExponentialHermite"],
+    ids=[
+        "B1",
+        "B2",
+        "B3",
+        "Exponential-M5-alpha2pi/M",
+        "Exponential-M10-alpha0.3",
+        "CatmullRom",
+        "CubicHermite",
+        "ExponentialHermite",
+    ],
 )
 def basis_function(request):
     basis_function, params = request.param
