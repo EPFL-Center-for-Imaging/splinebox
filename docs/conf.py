28754e8a2ac9e6b9b0046c1efcba09c4fb22acf1
faymanns <florian.aymanns@t-online.de>
2024-06-13 12:27:02 +0200

    Add background to docs

--- docs/conf.py
+++ docs/conf.py
@@ -104,6 +104,7 @@ html_context = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_css_files = ["css/custom.css"]
 
 # Example gallery configuration
 sphinx_gallery_conf = {
---
+++ examples/plot_background.py
@@ -0,0 +1,41 @@
+"""
+Generate the splinebox documentation background
+-----------------------------------------------
+
+This example shows how the background of the splinebox documentation is made.
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import splinebox
+import splinebox.basis_functions
+import splinebox.spline_curves
+
+knots = np.array([[0, 1], [0.1, 0.9], [0.2, 0.8], [0.6, 0.7], [0.35, 0.4], [0.8, 0.2], [0.9, 0.1], [1, 0]])
+M = len(knots)
+
+spline = splinebox.Spline(M=M, basis_function=splinebox.B3(), closed=False)
+spline.knots = knots
+
+ts = np.linspace(0, M - 1, 100)
+
+fig = plt.figure(frameon=False)
+fig.set_size_inches(2, 2)
+ax = plt.Axes(fig, [0.0, 0.0, 1.0, 1.0])
+ax.axis("equal")
+ax.set_axis_off()
+fig.add_axes(ax)
+control_points = np.clip(spline.control_points, 0, 1)
+ax.plot(
+    control_points[1:-1, 0],
+    control_points[1:-1, 1],
+    "-o",
+    color="forestgreen",
+    linewidth=1,
+    markersize=1,
+    alpha=0.07,
+)
+vals = spline.eval(ts)
+ax.plot(vals[:, 0], vals[:, 1], "-", color="forestgreen", linewidth=1, alpha=0.07)
+plt.savefig("../docs/_static/background.svg", bbox_inches="tight")
+plt.show()
