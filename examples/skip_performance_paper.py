ee65be45eb9bae25589485680ddf31fa005ec758
aymanns <florian.aymanns@epfl.ch>
2026-02-02 15:05:17 +0100

    Change fit to sparse matrix and remove _get_tval and _wrap_index

--- examples/skip_performance_paper.py
+++ examples/skip_performance_paper.py
@@ -19,6 +19,7 @@ nt = 1000
 
 results_creation = []
 results_evaluation = []
+results_fitting = []
 
 for repetition in range(n_repetitions + 1):
     knots = np.random.rand(M, ndim)
@@ -37,7 +38,7 @@ for repetition in range(n_repetitions + 1):
             ]
         )
 
-    # Measure spline evaluation time for different number of parameter values (nt)
+    # Measure splinebox evaluation time
     t = np.linspace(0, M if closed else M - 1, nt)
     start_eval = time.perf_counter_ns()
     spline(t)
@@ -48,6 +49,23 @@ for repetition in range(n_repetitions + 1):
             [
                 "splinebox",
                 (stop_eval - start_eval) / 1000,
+            ]
+        )
+
+    # Measure splinebox fitting time
+
+    data = np.random.rand(M * 10, ndim)
+
+    start = time.perf_counter_ns()
+    spline = splinebox.Spline(M, splinebox.B3(), closed=closed)
+    spline.fit(data)
+    stop = time.perf_counter_ns()
+
+    if repetition != 0:
+        results_fitting.append(
+            [
+                "splinebox",
+                (stop - start) / 1000,
             ]
         )
 
@@ -85,6 +103,40 @@ for repetition in range(n_repetitions + 1):
             ]
         )
 
+    # Measure scipy fitting time
+    # The `splprep` is declared as legacy by scipy.
+    # However, neither `make_splprep` nor `make_lsq_spline` seem to be able to do the same thing.
+    k = 3
+    if closed:
+        start = time.perf_counter_ns()
+        N = len(data)
+        t = np.arange(-k, M + k + 1)
+        u = np.linspace(0, M, N + 1)[:-1]
+        tck, u = scipy.interpolate.splprep(data.T, k=k, u=u, t=t, task=-1, s=0, per=N)
+        t, c, k = tck
+        c = np.stack(c, axis=-1)
+        spline = scipy.interpolate.BSpline(t, c, k)
+        stop = time.perf_counter_ns()
+
+    else:
+        start = time.perf_counter_ns()
+        N = len(data)
+        t = np.arange(-k, M + k)
+        u = np.linspace(0, M - 1, N)
+        tck, u = scipy.interpolate.splprep(data.T, k=k, u=u, t=t, task=-1, s=0)
+        t, c, k = tck
+        c = np.stack(c, axis=-1)
+        spline = scipy.interpolate.BSpline(t, c, k)
+        stop = time.perf_counter_ns()
+
+    if repetition != 0:
+        results_fitting.append(
+            [
+                "scipy",
+                (stop - start) / 1000,
+            ]
+        )
+
 creation_df = pd.DataFrame(results_creation, columns=["Package", "time [micros]"])
 creation_df = creation_df.groupby("Package", as_index=False).agg(["mean", "std"])
 print()
@@ -99,57 +151,6 @@ print("##########################################")
 print("Evaluation")
 print("##########################################")
 print(evaluation_df)
-exit()
-
-results_fitting = []
-
-for repetition in range(n_repetitions + 1):
-    data = np.random.rand(M * 10, ndim)
-
-    # Measure splinebox fitting time
-    start = time.perf_counter_ns()
-    spline = splinebox.Spline(M, splinebox.B3(), closed=closed)
-    spline.fit(data)
-    stop = time.perf_counter_ns()
-
-    if repetition != 0:
-        results_fitting.append(
-            [
-                "splinebox",
-                (stop - start) / 1000,
-            ]
-        )
-
-    # Measure scipy fitting time
-    k = 3
-    if closed:
-        start = time.perf_counter_ns()
-        N = len(data)
-        t = np.arange(-k, M + k + 1)
-        u = np.linspace(0, M, N + 1)[:-1]
-        tck, u = scipy.interpolate.splprep(data.T, k=k, u=u, t=t, task=-1, s=0, per=N)
-        stop = time.perf_counter_ns()
-
-    else:
-        start = time.perf_counter_ns()
-        N = len(data)
-        t = np.arange(-k, M + k)
-        u = np.linspace(0, M - 1, N)
-        tck, u = scipy.interpolate.splprep(data.T, k=k, u=u, t=t, task=-1, s=0)
-        stop = time.perf_counter_ns()
-
-    if repetition != 0:
-        results_fitting.append(
-            [
-                "scipy",
-                (stop - start) / 1000,
-            ]
-        )
-
-fitting_df = pd.DataFrame(
-    results_fitting,
-    columns=["Package", "time [micros]"],
-)
 fitting_df = pd.DataFrame(results_fitting, columns=["Package", "time [micros]"])
 fitting_df = fitting_df.groupby("Package", as_index=False).agg(["mean", "std"])
 print()
